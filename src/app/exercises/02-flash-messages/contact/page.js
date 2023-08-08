ddc59f7d47c75c20885bc9ea5cf93ea77bf62530
Ayuth Mangmesap <ayuth.mang@gmail.com>
2023-08-08 15:56:23 +0700

    Solution for 02-flash-messages

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,19 +1,37 @@
 'use client';
 import React from 'react';
+import {ToastContext} from '../../../../components/ToastProvider';
+import { useRouter } from 'next/navigation';
 
 function ContactPage() {
+  const {createToast} = React.useContext(ToastContext)
+  const router = useRouter()
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+
+    // TODO: Send the data to the server
+
+    router.push('/exercises/02-flash-messages')
+
+    createToast(
+      "Your message was received. We'll get back to you shortly!",
+      'success'
+    )
+  }
+
   return (
-    <main>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input id="name" required={true} />
+      <main>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input id="name" required={true} />
 
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" />
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" />
 
-        <button>Submit</button>
-      </form>
-    </main>
+          <button>Submit</button>
+        </form>
+      </main>
   );
 }
 
